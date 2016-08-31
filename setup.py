73ec308e65d5cc2c342c5b4f484823f20ca083b3
Jonathan Kummerfeld <jkk@berkeley.edu>
2016-08-31 14:29:20 -0400

    Adding package related files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='berkeley_parse_analyser',
+      version='1.0',
+      url='https://github.com/jkkummerfeld/berkeley-parser-analyser/',
+      author='Jonathan K. Kummerfeld',
+      author_email='jkk@berkeley.edu',
+      license='ISC',
+      description='A tool for classifying mistakes in the output of syntactic parsers.',
+      packages=['berkeley_parse_analyser'])
